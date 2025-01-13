7e4fe3588d27927a6f72ffcf1d0a1848de850f8f
Aroonay Anand <98151754+Aroonay@users.noreply.github.com>
2025-01-13 22:15:17 +0530

    Add files via upload

---
+++ RatingService/src/main/java/com/app/rating/repository/RatingRepository.java
@@ -0,0 +1,12 @@
+package com.app.rating.repository;
+
+import com.app.rating.entities.Rating;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RatingRepository extends JpaRepository<Rating, String> {
+
+    List<Rating> findByUserId(String userId);
+    List<Rating> findByHotelId(String hotelId);
+}
